7aa94f2c57c395c0d746119567e3d4618d4fb46a
skarj <skaarj.sergey@gmail.com>
2025-03-19 20:35:07 +0300

    Fix deploys to render.com

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install
+
+# TODO: fix
+uv pip compile pyproject.toml -o requirements.txt
+pip install -r requirements.txt
